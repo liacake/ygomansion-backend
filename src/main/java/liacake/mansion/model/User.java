0765d42b108b1edd5fb7a34cd81fa0aff58a3c44
liacake <119245595+liacake@users.noreply.github.com>
2026-05-10 11:38:17 +0100

    models - add User and Card entities

--- src/main/java/liacake/mansion/model/User.java
+++ src/main/java/liacake/mansion/model/User.java
@@ -1,6 +1,5 @@
 package liacake.mansion.model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import java.util.Set;
 
@@ -23,80 +22,20 @@ public class User {
     @Column(name = "role")
     private Set<String> roles;
 
-    @Column(length = 1000)
-    private String description;
-
     private String favouriteCard;
 
-    @Column(columnDefinition = "TEXT")
     private String imageUrl;
 
-    @OneToMany(mappedBy = "owner")
-    @JsonManagedReference
-    private Set<Card> cards;
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Set<String> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<String> roles) {
-        this.roles = roles;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getFavouriteCard() {
-        return favouriteCard;
-    }
-
-    public void setFavouriteCard(String favouriteCard) {
-        this.favouriteCard = favouriteCard;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public Set<Card> getCards() {
-        return cards;
-    }
-
-    public void setCards(Set<Card> cards) {
-        this.cards = cards;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    public Set<String> getRoles() { return roles; }
+    public void setRoles(Set<String> roles) { this.roles = roles; }
+    public String getFavouriteCard() { return favouriteCard; }
+    public void setFavouriteCard(String favouriteCard) { this.favouriteCard = favouriteCard; }
+    public String getImageUrl() { return imageUrl; }
+    public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
 }
