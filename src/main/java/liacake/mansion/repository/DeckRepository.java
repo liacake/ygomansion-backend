b76b71faf7528ff00cde702aabfd999df37a1715
liacake <119245595+liacake@users.noreply.github.com>
2026-05-10 16:22:09 +0100

    repositories - add JPA repositories for User, Card, Deck

--- src/main/java/liacake/mansion/repository/DeckRepository.java
+++ src/main/java/liacake/mansion/repository/DeckRepository.java
@@ -5,20 +5,11 @@ import liacake.mansion.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
-@Repository
 public interface DeckRepository extends JpaRepository<Deck, Long> {
-    List<Deck> findByOwner(User user);
+    List<Deck> findByOwner(User owner);
 
-    // Custom query to find decks containing a specific local card ID string
-    // This performs a join on the 'local_cards' element collection table.
-    @Query("SELECT DISTINCT d FROM Deck d JOIN d.localcards lc WHERE lc = :cardIdStr")
-    List<Deck> findDecksByLocalCardString(@Param("cardIdStr") String cardIdStr);
-
-    // Custom query to find decks containing a specific YGOPRODECK card ID string
-    @Query("SELECT DISTINCT d FROM Deck d JOIN d.ygoprocards yc WHERE yc = :cardIdStr")
-    List<Deck> findDecksByYgoproCardString(@Param("cardIdStr") String cardIdStr);
+    @Query("SELECT d FROM Deck d JOIN d.localcards c WHERE c = :cardId")
+    List<Deck> findDecksByLocalCardString(@Param("cardId") String cardId);
 }
--- src/main/java/liacake/mansion/repository/UserRepository.java
+++ src/main/java/liacake/mansion/repository/UserRepository.java
@@ -2,7 +2,6 @@ package liacake.mansion.repository;
 
 import liacake.mansion.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
